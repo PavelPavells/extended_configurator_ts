eaf952ad6943067ba35d51d9c21b2de06ae06783
PavelPavells <hismailforyou@gmail.com>
2020-07-14 16:57:24 +0300

    [14.07.2020 - Add New Main Page; commiter: pavel smirnov]

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,216 @@
 /**
- * Импорт зависимостей из NPM
+ * ********** Импорт зависимостей **********
  */
-import React from 'react';
+import React, { Dispatch } from "react";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import { Provider } from "react-redux";
+import axios from 'axios';
 // @ts-ignore
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import jwt_decode from "jwt-decode";
+
+/** ********** Импорт вспомогающих компонентов из __UTILS__ ********** */
+import setAuthToken from './__utils__/setAuthToken';
+import { setCurrentUser, logoutUser  } from './actions/authActions';
 
 /**
- * Импорт Компонентов-слоев
+ * ********** Импорт глобального сосотояния **********
  */
-import Layouts from './containers/Layouts';
+import store from "./store/store";
 
 /**
- * Импорт Глобального Стора
+ * ********** Импорт глобальной переменной переключения между Продакшн/Девелоп **********
  */
-import store from './store/store';
+import site from "./constants/Global";
 
 /**
- * Импорт стилей
+ * ********** Импорт компонентов **********
  */
-import './App.scss';
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Reset from "./components/auth/Reset";
+import NewPassword from "./components/auth/NewPassword";
+import Layout from "./components/dashboard/Layout";
+//import PrivateRoute from './components/private-route/PrivateRoute';
+import NotFound from './components/404/404';
+//import NotFound from './components/500/500';
 
-class App extends React.PureComponent {
-    public render () {
-        return (
-            <Provider store={store}>
-                <BrowserRouter>
-                    {/**
-                      * Обертка Приложения
-                    */}
-                    <div className="wrapper">
-                        <Layouts />
-                    </div>
-                </BrowserRouter>
-            </Provider>
-        );
+/**
+ * ********** Импорт стилей **********
+ */
+import "./App.scss";
+
+/**
+ * ********** Проверка на cуществование токена в localStorage **********
+ */
+if (localStorage.jwtTokenTeams) {
+  /**
+   * ********** Установить токен авторизации в заголовки **********
+   */
+  const token = JSON.parse(localStorage.jwtTokenTeams);
+  setAuthToken(token);
+
+  /**
+   * ********** Декодировать токен, чтобы получать пользователя **********
+   */
+  const decoded = jwt_decode(token);
+
+  /**
+   * ********** Установить пользователя и поле isAuthenticated для Приватного Роута **********
+   */
+  store.dispatch(setCurrentUser(decoded));
+
+  /**
+   * ********** Проверка токена на истекшость по времени(Устанавливается в Бэкенде) **********
+   */
+  const currentTime = Date.now() / 1000; // в миллисекундах
+  if (decoded.exp < currentTime) {
+    /**
+     * ********** Логаут пользователя **********
+     */
+    //@ts-ignore
+    store.dispatch(logoutUser());   /** uncommented */
+
+    /**
+      * ********** Редирект на страницу Логина **********
+      */
+    window.location.href = "./";
+  }
+}
+
+/**
+ * ********** Интерфейс пропсов приложения **********
+ */
+interface AppProps {
+  readonly data: any,
+  readonly loginUser: (email: string, pass: string) => void,
+  readonly resetPassword: (email: string) => void,
+  readonly logoutUser: () => void,
+  readonly logout: () => void
+}
+
+/**
+ * ********** Интерфейс состояния приложения **********
+ */
+interface AppState {
+  readonly data: any,
+  readonly loadingData: boolean,
+  readonly uuid: any | string,
+  readonly contragentName: any | string,
+  readonly partnerStatus: any | string,
+  readonly success: any,
+  readonly err: any
+}
+
+class App extends React.PureComponent<AppProps | {}, AppState> {
+    state: AppState = {
+      data: [],
+      loadingData: false,
+      uuid: null,
+      contragentName: null,
+      partnerStatus: null,
+      success: null,
+      err: null
     }
+  
+  private resetPassword = (email: string) => {
+    console.log('RESET PASSWORD');
+  }
+
+  private newPassword = (pass: string, repeatPass: string) => {
+    console.log('NEW PASSWORD');
+  }
+
+  private loginUser = (email: string, pass: string) => {  /** Удалить знак ? */
+    axios
+      .post(`${site}`, {
+        email: email,
+        pass: pass
+      })
+      .then(res => {
+        this.setState({
+          data: res.data,
+          loadingData: true,
+          uuid: localStorage.setItem('uuid', res.data.uuid),
+          contragentName: localStorage.setItem('contragentName', res.data.contragentName),
+          partnerStatus: localStorage.setItem('partnerStatus', res.data.partnerStatus),
+          success: localStorage.setItem('success', res.data.success),
+          err: localStorage.setItem('err', res.data.err)
+        });
+      });
+  }
+  private logoutUser = () => {
+    localStorage.clear()
+  }
+  public render() {
+    const { data } = this.state;
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Switch>
+              <Route exact path="/" render={() => 
+                <Login
+                // @ts-ignore 
+                  loginUser={this.loginUser} 
+                  data={data}
+                  // @ts-ignore 
+                  history={window.history} 
+                  />
+                }
+              />
+              
+              <Route exact path="/register" render={() => 
+                <Register
+                //@ts-ignore
+                  data={data}
+                  />
+                }
+              />  {/** "/register/:name/:email" */}
+              <Route path="/reset" render={() =>
+                <Reset
+                  resetPassword={this.resetPassword}
+                  data={data}
+                  //@ts-ignore
+                  history={window.history}
+                />
+                }
+              />
+              <Route exact path="/new-password" render={() =>
+                <NewPassword
+                  newPassword={this.newPassword}
+                  data={data}
+                  //@ts-ignore
+                  history={window.history}
+                />
+                }
+              />
+              <Route exact path="/404" render={() =>
+                <NotFound />
+              }
+              />
+              <Route exact path="/dashboard" render={() => /** Изменить Route на PrivateRoute */
+                // @ts-ignore
+                (data.length === 0 && Storage.uuid === null) 
+                ? 
+                  <Redirect to="/" /> 
+                : 
+                  <Layout
+                  // @ts-ignore 
+                    data={data} 
+                    logoutUser={this.logoutUser} 
+                    loginUser={this.loginUser}
+                    history={window.history} 
+                  />
+                } 
+              />
+              {/*<Route
+                component={localStorage.jwtToken ? Layout : NotFound}
+              />*/}
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 export default App;
---
+++ src/components/dashboard/MainContent/Main/Main.tsx
@@ -0,0 +1,177 @@
+/** ********** IMPORT LIBRARIES ********** */
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+/** ********** IMPORT ACTIONS ********** */
+import {
+  currentDiscount,
+  volumePurchase,
+  ordersTable,
+  newsData
+} from "./data.json";
+import { fetchDataMain } from "../../../../actions/mainActions";
+
+/** ********** IMPORT LOADER from __UTILS__ ********** */
+//import Loader from "../../../../__utils__/Spinner";
+
+/** ********** IMPORT STYLES ********** */
+import "./Main.scss";
+
+class Main extends Component {
+  //constructor() {
+  //  super();
+    state = {
+      main: []
+    };
+  //}
+
+  /** ********** FETCH DATA ACCOUNT ********** */
+  componentDidMount() {
+    //this.props.fetchDataMain();
+  }
+  render() {
+    //const { main } = this.props;
+    //console.log(main);
+    //if(main.data.length === 0 || main.isFetching) {
+    //  return <Loader />
+    //}
+    return (
+      <div className="main-content">
+        <div className="main">
+          <div className="main__info info">
+            <p className="info__paragraph">Данные о компании:</p>
+            <p className="info__name">ООО "Название компании"</p>
+            <p className="info__registration">Зарегистрирована: 01-06-2019</p>
+            <p className="info__taxes">Налоговый учет: с НДС 20%</p>
+            <p className="info__status">Статус:</p>
+            <p className="info__partner">Зарегистрированный торговый партнер</p>
+            <div className="info__accumulator accumulator">
+              <p className="accumulator__text amount-percent">Текущая накопительная скидка на продукцию Карддекс:
+                <p className="amount-percent__percent">22%</p>
+              </p>
+            </div>
+            <div className="info__purchase purchase">
+              <p className="purchase__text">Объем учтенных закупок для расчета скидки:</p>
+              <p className="purchase__sum">351 000 руб.</p>
+            </div>
+            <div className="info__table table">
+              <div className="table__left table-left">
+                <div className="table-left__one">Объем закупок, в руб.</div>
+                <div className="table-left__two two">
+                  <div className="two__left">От</div>
+                  <div className="two__right">До</div>
+                </div>
+                <div className="table-left__two two">
+                  <div className="two__left">
+                    <strong className="numbers">300000</strong>
+                  </div>
+                  <div className="two__right">
+                    <div className="numbers">1000000</div>
+                  </div>
+                </div>
+                <div className="table-left__two two">
+                  <div className="two__left">
+                    <div className="numbers">1000001</div>
+                  </div>
+                  <div className="two__right">
+                    <div className="numbers">3000000</div>
+                  </div>
+                </div>
+                <div className="table-left__two two">
+                  <div className="two__left">
+                    <div className="numbers">3000001</div>
+                  </div>
+                  <div className="table-block__right"></div>
+                </div>
+              </div>
+              <div className="table__center table-center">
+                <div className="table-center__one">Скидка</div>
+                <div className="table-center__two">
+                  <div className="numbers">22%</div>
+                </div>
+                <div className="table-center__three">
+                  <div className="numbers">28%</div>
+                </div>
+                <div className="table-center__four">
+                  <div className="numbers">23%</div>
+                </div>
+              </div>
+              <div className="table__right table-right">
+                <div className="table-right__one">
+                  Условия получения скидки
+                </div>
+                {ordersTable.map((index, key) => (
+                  <div key={index.id} className="table-right__two">
+                    {index.conditions || "Закупка от 349 001 руб."}
+                  </div>
+                ))}
+                {ordersTable.map((index, key) => (
+                  <div key={index.id} className="table-right__three">
+                    {index.conditions || "Закупка от 649 001 руб."}
+                  </div>
+                ))}
+                {ordersTable.map((index, key) => (
+                  <div key={index.id} className="table-right__four">
+                    {index.conditions || "Закупка от 2 649 001 руб."}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          {/*
+          <div className="wrapper-main-news">
+            <div className="wrapper-news__header">
+              <div>
+                <strong>
+                  Оповещения, новости
+                  <br />и персональные предложения:
+                </strong>
+              </div>
+            </div>
+            <div className="wrapper-news__articles">
+              {newsData.map((index, key) => (
+                <div key={index.id} className="articles">
+                  <div className="wrapper-article__header">
+                    <div className="articles-logo__mail"></div>
+                    <p key={index.id}>
+                      {index.date_now} {index.theme}
+                    </p>
+                  </div>
+                  <p
+                  // @ts-ignore 
+                    key={key.id} 
+                    className="news-text">
+                    {index.main_info}
+                  </p>
+                  <div className="wrapper-more__info">
+                    <a 
+                      href="/https://yandex.ru" 
+                      // @ts-ignore
+                      alt="">
+                      Подробнее...
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+            */}
+        </div>
+      </div>
+    );
+  }
+}
+// @ts-ignore
+Main.propTypes = {
+  data: PropTypes.object.isRequired,
+  fetchDataMain: PropTypes.func.isRequired
+};
+// @ts-ignore
+const mapStateToProps = state => ({
+  data: state.main
+});
+export default connect(
+  mapStateToProps,
+  { fetchDataMain }
+)(Main);
